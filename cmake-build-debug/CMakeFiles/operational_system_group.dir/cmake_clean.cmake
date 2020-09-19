de47e4a5cac41a0abcd7062c3607a904a31d3e88
elivelton <eliveltonrangel@gmail.com>
2020-09-19 08:28:50 -0300

    Added new functions

---
+++ cmake-build-debug/CMakeFiles/operational_system_group.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/operational_system_group.dir/main.c.o"
+  "operational_system_group"
+  "operational_system_group.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/operational_system_group.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
